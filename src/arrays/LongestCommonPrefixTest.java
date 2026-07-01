007d773dbc55631a837b18e57be153b8b30cb47d
Darion <darionsayles06@gmail.com>
2026-07-01 15:51:46 -0400

    Initial LeetCode project setup

---
+++ src/arrays/LongestCommonPrefixTest.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class LongestCommonPrefixTest {
+    public static void main(String[] args) {
+        LongestCommonPrefix solution = new LongestCommonPrefix();
+
+        String[] input = {"flower","flow","flight"};
+
+        System.out.println(solution.longestCommonPrefix(input));
+    }
+}
